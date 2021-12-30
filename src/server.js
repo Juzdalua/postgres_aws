3aa7379de06280bc17795a72ff93da80b7ac5cb3
Jufien <kimjun47@newbizstart.com>
2021-12-30 12:20:55 +0900

    update with postgresql

--- src/server.js
+++ src/server.js
@@ -1,6 +1,34 @@
-import express from "express";
 // const express = require("express");
+import express from "express";
 import bodyParser from "body-parser";
+
+//postgresql
+const {Client} = require('pg');
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: '78xhdtls@',
+    port: 5432,
+});
+client.connect( error => {
+    if(error)
+        console.log(error);
+    else
+        console.log("seccess.");
+});
+
+/*
+client.query('SELECT *FROM users', (error, res)=> {
+    console.log(`error: ${error}`);
+    console.log(`response: ${JSON.stringify(res)}`);
+    console.log(res.rows);
+    console.log(res.rows[0].name);
+    console.log(res.rows[0].age);
+    console.log(res.rows.length);
+    client.end();
+});
+*/
 
 const app = express();
 const PORT = 4000;
@@ -8,12 +36,13 @@ const PORT = 4000;
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/", (req, res) => {
+    
     return res.sendFile(__dirname+"/index.html");
 });
 
 app.post("/", (req, res) => {
-    const {text} = req.body;
-    return res.send(`${text}가 입력되었습니다.`);
+    const {name, age} = req.body;    
+    return res.send(`${name}님은 ${age}세입니다.`);
 })
 
 app.listen(PORT, () => {
